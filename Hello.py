7d1f556dd2bb96222f194b3d5f5eef8fdf578d4f
Aditya Kumar Singh <aditya.singh@xceedance.com>
2021-07-18 22:33:44 +0530

    Simple Flask App

--- Hello.py
+++ Hello.py
@@ -1,3 +1,17 @@
-print("Hello World")
+from flask import Flask, request
 
-print("Hey this is my first program for GitHub")
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+@app.route("/home", methods = ["GET", "POST"])
+def hello_world():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        return "hello its from POST"
+    else:
+        return "Hello its from Get"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port = 2021)
